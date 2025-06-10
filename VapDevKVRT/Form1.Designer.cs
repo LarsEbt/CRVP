cb6dada3430314efdca8a54f01c99e031375f1d5
LarsEbt <larsebert0104@gmail.com>
2025-06-10 19:55:59 +0200

    PoC Form

--- VapDevKVRT/Form1.Designer.cs
+++ VapDevKVRT/Form1.Designer.cs
@@ -28,12 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            OpenFile = new Button();
+            panel1 = new Panel();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // OpenFile
+            // 
+            OpenFile.Location = new Point(21, 25);
+            OpenFile.Name = "OpenFile";
+            OpenFile.Size = new Size(127, 23);
+            OpenFile.TabIndex = 0;
+            OpenFile.Text = "Open File";
+            OpenFile.UseVisualStyleBackColor = true;
+            OpenFile.Click += button1_Click;
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(21, 54);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1000, 1000);
+            panel1.TabIndex = 1;
+            panel1.Paint += panel1_Paint;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(281, 25);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(740, 23);
+            textBox1.TabIndex = 2;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(103, 1060);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(703, 1060);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 4;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(23, 1065);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 5;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(638, 1068);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 5;
+            label2.Text = "label1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1041, 1091);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(panel1);
+            Controls.Add(OpenFile);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button OpenFile;
+        private Panel panel1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label1;
+        private Label label2;
     }
 }
--- VapDevKVRT/Form1.cs
+++ VapDevKVRT/Form1.cs
@@ -2,9 +2,66 @@ namespace VapDevKVRT
 {
     public partial class Form1 : Form
     {
+
+        private CVRPSolution Solution;
+
+        private CVRPInstance Instance;
+
+        private List<Brush> BrushList = new List<Brush>
+        {
+            Brushes.Red,
+            Brushes.Green,
+            Brushes.Blue,
+            Brushes.Yellow,
+            Brushes.Orange,
+            Brushes.Purple,
+            Brushes.Cyan,
+            Brushes.Magenta,
+            Brushes.Brown,
+            Brushes.LimeGreen,
+            Brushes.Aqua,
+            Brushes.Beige,
+            Brushes.Bisque,
+            Brushes.Black,
+            Brushes.Chartreuse,
+            Brushes.Chocolate,
+            Brushes.Coral,
+            Brushes.DarkBlue,
+            Brushes.DarkCyan,
+            Brushes.DarkGoldenrod,
+            Brushes.DarkGreen,
+            Brushes.DarkMagenta,
+            Brushes.DarkOrange,
+            Brushes.DarkOrchid,
+            Brushes.DarkRed,
+            Brushes.DarkSalmon,
+            Brushes.DarkSlateGray
+        };
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
