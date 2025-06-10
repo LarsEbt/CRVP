3a5d0e50e585cb5cd784a88fac35985054aff69c
Moritz Ehrhardt <moritz.ehrhardt@uni-jena.de>
2025-06-10 13:06:34 +0200

    gurobi implementierung

--- VapDevKVRT/Program.cs
+++ VapDevKVRT/Program.cs
@@ -8,23 +8,44 @@ namespace VapDevKVRT
         [STAThread]
         static void Main()
         {
-
             RunBeforeGUI();
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
-
-
-
         }
 
 
         public static void RunBeforeGUI()
         {
+            //1. Instanz generieren
             Console.WriteLine("Running pre-GUI setup...");
             InstanceGenerator generator = new InstanceGenerator();
             generator.GenerateInstances(10, 5, 20); // Example parameters: 10 instances, 5 vehicles, 20 demand locations
+
+            //2. Instanzname definieren
+            string instanceName = "0-5-20"; // Example instance name
+
+            //3. Instancz laden
+            CVRPInstance instance = CVRPInstance.ReadFromFile(instanceName);
+
+            //4. Sovler wählen
+            ISolver sovler = new GurobiSolver(instance, 60);
+
+            //5. Instanz lösen
+            CVRPSolution solution = sovler.Solve();
+
+
+            // 6. Lösung speichern
+            solution.WriteToFile();
+
+            // 7. Ausgabe in Konsole
+            Console.WriteLine("----- Lösung abgeschlossen -----");
+            Console.WriteLine($"Solver: {solution.Solver}");
+            Console.WriteLine($"Kosten: {solution.DeliveryCosts:F2}");
+            Console.WriteLine($"Fahrzeuge: {solution.NumberOfVehicles}");
+            Console.WriteLine($"Lösungszeit: {solution.Solutiontime:F3} Sekunden");
+            Console.WriteLine("--------------------------------");
 
 
         }
