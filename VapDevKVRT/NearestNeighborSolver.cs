e7e2d9e0fcf9bda1d35da4646fee5bad6df7d0fa
ArtemisgitHu <jonassalzmann@uni-jena.de>
2025-06-10 22:28:03 +0200

    update

--- VapDevKVRT/NearestNeighborSolver.cs
+++ VapDevKVRT/NearestNeighborSolver.cs
@@ -1,89 +1,84 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-public class NearestNeighborSolver
+namespace VapDevKVRT
 {
-    public CVRPSolution Solve(CVRPInstance instance, int vehicleCapacity)
+    public class NearestNeighborSolver
     {
-    var stopwatch = System.Diagnostics.Stopwatch.StartNew();
-
-    int n = instance.NumberOfDemandLocations;
-    bool[] visited = new bool[n];
-    var remainingDemand = instance.d.ToArray();
-
-    var warehouse = instance.warehouse;
-    var customers = instance.CoordinatesCustomers;
-
-    var allRoutes = new List<List<int>>();
-    double totalCost = 0;
-    int vehiclesUsed = 0;
-
-    while (visited.Any(v => !v))
-    {
-        var route = new List<int>();
-        double remainingCapacity = vehicleCapacity;
-        var currentLocation = warehouse;
-        double routeCost = 0;
-
-        while (true)
+        public CVRPSolution Solve(CVRPInstance instance, int vehicleCapacity)
         {
-            int nearest = -1;
-            double minDistance = double.MaxValue;
+            var stopwatch = Stopwatch.StartNew();
 
-            for (int i = 0; i < n; i++)
+            int n = instance.NumberOfDemandLocations;
+            bool[] visited = new bool[n]; // für Kunden 1 bis n (Index 0 bis n-1)
+            var remainingDemand = instance.d.ToArray();
+
+            var allRoutes = new List<List<int>>();
+            double totalCost = 0;
+            int vehiclesUsed = 0;
+
+            while (visited.Any(v => !v))
             {
-                if (!visited[i] && remainingDemand[i] <= remainingCapacity)
+                var route = new List<int> { 0 }; // Start beim Depot
+                double remainingCapacity = vehicleCapacity;
+                int currentLocationIndex = 0; // Depot = 0
+                double routeCost = 0;
+
+                while (true)
                 {
-                    double dist = Distance(currentLocation, customers[i]);
-                    if (dist < minDistance)
+                    int nearest = -1;
+                    double minDistance = double.MaxValue;
+
+
+                    for (int i = 0; i < n; i++)
                     {
-                        minDistance = dist;
-                        nearest = i;
+                        if (!visited[i] && remainingDemand[i] <= remainingCapacity)
+                        {
+                            double dist = instance.DistanceMatrix[currentLocationIndex, i + 1];
+
+                            if (dist < minDistance)
+                            {
+                                minDistance = dist;
+                                nearest = i;
+                            }
+                        }
                     }
+
+                    if (nearest == -1)
+                        break;
+
+                    // Gehe zum Kunden
+                    route.Add(nearest + 1); // +1, weil Kunde i+1 in Matrix
+                    visited[nearest] = true;
+                    remainingCapacity -= remainingDemand[nearest];
+                    routeCost += instance.DistanceMatrix[currentLocationIndex, nearest + 1];
+                    currentLocationIndex = nearest + 1;
                 }
+
+                // Rückfahrt zum Depot
+                route.Add(0);
+                routeCost += instance.DistanceMatrix[currentLocationIndex, 0];
+
+                allRoutes.Add(route);
+                totalCost += routeCost;
+                vehiclesUsed++;
             }
 
-            if (nearest == -1)
-                break; // Keine passenden Kunden mehr für diese Tour
+            stopwatch.Stop();
 
-            // Besuch einplanen
-            route.Add(nearest);
-            visited[nearest] = true;
-            remainingCapacity -= remainingDemand[nearest];
-            routeCost += Distance(currentLocation, customers[nearest]);
-            currentLocation = customers[nearest];
-        }
+            var solution = new CVRPSolution(
+                instance.Name,
+                "NearestNeighbor",
+                totalCost,
+                stopwatch.Elapsed.TotalSeconds,
+                vehiclesUsed
+            );
+            solution.Routes = allRoutes;
+            solution.WriteToFile();
 
-        // Rückfahrt zum Lager
-        routeCost += Distance(currentLocation, warehouse);
-
-        totalCost += routeCost;
-        allRoutes.Add(route);
-        vehiclesUsed++;
-    }
-
-    stopwatch.Stop();
-
-    var solution = new CVRPSolution(
-        instance.Name,
-        "Nearest Neighbor",
-        totalCost,
-        stopwatch.Elapsed.TotalSeconds,
-        vehiclesUsed,
-        allRoutes // optional, falls CVRPSolution das unterstützt
-    );
-
-    solution.WriteToFile();
-        return solution;
-        }
-    
-        private double Distance((double x, double y) a, (double x, double y) b)
-        {
-            double dx = a.x - b.x;
-            double dy = a.y - b.y;
-            return Math.Sqrt(dx * dx + dy * dy);
+            return solution;
         }
     }
+}
